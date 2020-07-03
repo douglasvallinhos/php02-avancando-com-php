bd98b034774ac62d5f00db50a7f4af82467641fb
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-02 22:34:49 -0300

    adicionado html com o php para mostrar no navegador as contas

--- banco.php
+++ banco.php
@@ -24,3 +24,30 @@ unset($contasCorrentes['123.456.789-12']);
 foreach ($contasCorrentes as $cpf => $conta){
     exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
 }
+
+?>
+
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta){ ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf;?></h3>
+        </dt>
+        <dd>
+            Saldo: <?=$conta['saldo'];?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
